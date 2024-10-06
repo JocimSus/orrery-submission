be544b93e0c29ea093c00fdac581880a1a1a5a61
marcoconc <101968044+marcoconc@users.noreply.github.com>
2024-10-06 19:39:19 +0700

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ const resetCamera = () => {
 
 const textureLoader = new THREE.TextureLoader()
 const ambient_light = new THREE.AmbientLight(0x444444)
-const sunlight = new THREE.PointLight(0xffffff, 1000000, 0, 1)
+const sunlight = new THREE.PointLight(0xffffff, 1000000, 0, 2)
 
 scene.add(ambient_light)
 scene.add(sunlight)
