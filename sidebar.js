cb4c906fd3835fd59b14e1c1b213093889040519
JOJO <jovanisk321@gmail.com>
2024-10-06 09:40:06 +0700

    facts

--- sidebar.js
+++ sidebar.js
@@ -2,7 +2,6 @@ const toggleButton = document.querySelectorAll(".category-toggle");
 
 toggleButton.forEach((e) => {
     e.addEventListener("click", () => {
-        // Toggle the class for the individual element 'e'
         if (e.classList.contains("on")) {
             e.classList.remove("on");
             e.classList.add("off");
@@ -17,46 +16,37 @@ toggleButton.forEach((e) => {
     });
 });
 
-var side_open = false; // Sidebar is initially closed
+var side_open = false;
 
 function slide() {
     const sidebar = document.querySelector('.sidebar');
     
     if (!side_open) {
-        sidebar.classList.add('visible'); // Show the sidebar
+        sidebar.classList.add('visible'); 
     } else {
-        sidebar.classList.remove('visible'); // Hide the sidebar
+        sidebar.classList.remove('visible'); 
     }
     
-    side_open = !side_open; // Toggle state
+    side_open = !side_open; 
 }
 
-/*const info_bar = document.querySelector(".information")
-const dummy = document.querySelector(".dummy-planet")
+const name = document.getElementById("planet-name")
+const size = document.getElementById("size")
+const sma = document.getElementById("sma")
+const oe = document.getElementById("oe")
+const rp = document.getElementById("rp")
+const period = document.getElementById("period")
+const inclination = document.getElementById("inclination")
 
-let planets = [dummy]
-let info_open = false
+import { planetParams } from "./data";
 
-planets.forEach((e)=>{
-    e.addEventListener("click", ()=>{
-        if (!info_open)
-            info_bar.classList.add("visible")
-        else{
-            info_bar.classList.remove("visible")
-        }
-        info_open = !info_open;
-    })
+Object.keys(planetParams).forEach(planet => {
+    const planetData = planetParams[planet];
+    name.innerText = planetData.tag
+    size.innerText = planetData.size
+    sma.innerText = planetData.smA
+    oe.innerText = planetData.oE
+    rp.innerText = planetData.rotationPeriod
+    period.innerText = planetData.period
+    inclination.innerText = planetData.inclination
 })
-
-const info_bar = document.querySelector(".information")
-const dummy = document.querySelector(".dummy-planet")
-var info_open = false
-
-dummy.addEventListener("click", ()=>{
-    if (!info_open)
-        info_bar.classList.add("visible")
-    else{
-        info_bar.classList.remove("visible")
-    }
-    info_open = !info_open;
-})*/
